393b69c78024be5b2afc88140179b286e619b842
JussGabz <gabrieli94@hotmail.co.uk>
2023-06-15 16:55:44 +0100

    exerise detail tweak

--- GymApp/exercises/views.py
+++ GymApp/exercises/views.py
@@ -1,25 +1,21 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Exercise
 
 
 def index(request):
     # Latest 5 Exercises
     latest_exercises = Exercise.objects.order_by("-date_added")[:5]
-
-    template = loader.get_template("index.html")
     context = {"latest_exercises": latest_exercises}
-
-    loader.get_template("index.html")
-
-
     # output = ", ".join([ex.exercise_name for ex in latest_exercises])
+    return render(request, "index.html", context)
 
-    return HttpResponse(template.render(context, request))
 
 def exercise_detail(request, exercise_id):
-    response = "This is the exercise detail for exercise: %s"
-    return HttpResponse(response % exercise_id)
+
+    exercise = get_object_or_404(Exercise, id=exercise_id)
+    context = {"exercise": exercise}
+    return render(request, "detail.html", context)
 
 def exercise_difficulty(request, difficulty):
     response = "This is the %s exercises"
