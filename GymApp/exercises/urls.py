9edc07325e3719546e6f1d8507cc2aca28ccf0a5
JussGabz <gabrieli94@hotmail.co.uk>
2023-06-15 22:00:04 +0100

    app_name exercise

--- GymApp/exercises/urls.py
+++ GymApp/exercises/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'exercises'
 urlpatterns = [
     path("", views.index, name='index'),
-    path("<int:exercise_id>/", views.exercise_detail),
+    path("exercise/<int:exercise_id>/", views.exercise_detail, name='exercise_detail'),
     path("<str:difficulty>/", views.exercise_difficulty),
 ]
